53d24fa6a37f6de05d10395944971d2bb8189d3f
JP <jphil.provost@outlook.com>
2026-02-10 17:56:00 -0500

    Added sniper

--- FPS_00820_ShootieGunGame/Assets/Scripts/WeaponSMG.cs
+++ FPS_00820_ShootieGunGame/Assets/Scripts/WeaponSMG.cs
@@ -4,33 +4,20 @@ public class WeaponSMG : WeaponBase
 {
     [SerializeField] private Ammo ammo;
     [SerializeField] private Raycaster raycaster;
-    
-    [Header("Rate of Fire")]
-    [SerializeField] private float roundsPerSecond = 1f;
 
-    private float timeBetweenShots;
-    private float fireTimer;
-    private bool canFire = true;
     private bool isFiring = false;
 
-    public bool CanFire => canFire;
-
     #region OVERRIDES
-    private void Awake()
-    {
-        timeBetweenShots = 1f / roundsPerSecond;
-    }
-
     public override void UpdateWeapon()
     {
         ammo.UpdateReload(Time.deltaTime);
-        UpdateFire(Time.deltaTime);
+        
 
-        if(CanFire && isFiring && !ammo.IsReloading)
+        if(isFiring && !ammo.IsReloading)
         {
             ammo.FireShot();
             raycaster.FireShot();
-            canFire = false;
+            
         }
     }
 
@@ -45,25 +32,9 @@ public class WeaponSMG : WeaponBase
     }
 
     public override void OnReload()
-    {
-        canFire = false;
+    {        
         ammo.StartReload();
     }
     #endregion
 
-    #region RATE OF FIRE
-
-    public void UpdateFire(float deltaTime)
-    {
-        if (CanFire)
-            return;
-
-        fireTimer += Time.deltaTime;
-        if (fireTimer >= timeBetweenShots)
-        {
-            canFire = true;
-        }
-    }
-
-    #endregion
 }
---
+++ FPS_00820_ShootieGunGame/Assets/Scripts/WeaponSniper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class WeaponSniper : WeaponBase
+{
+    [SerializeField] Ammo ammo;
+    [SerializeField] Raycaster raycaster;
+    [SerializeField] RateOfFire RoF;
+
+    public override void UpdateWeapon()
+    {
+        //Update reload timer
+        RoF.UpdateFire(Time.deltaTime);
+        ammo.UpdateReload(Time.deltaTime);
+    }
+
+    public override void OnFirePressed()
+    {
+        //If we have ammo and we are not reloading
+        //Fire a shot
+        if (RoF.CanFire && ammo.HasAmmo() && !ammo.IsReloading)
+        {
+            ammo.FireShot();
+            raycaster.FireShot();
+            RoF.FireShot();
+        }
+    }
+
+    public override void OnFireReleased()
+    {
+
+    }
+
+    public override void OnReload()
+    {
+        ammo.StartReload();
+    }
+}
